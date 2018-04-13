b9d65a516541264ba7960597a292914b0950abfa
Evgenii KRUHLIUK <ekruhliu@e1r6p2.unit.ua>
2018-04-13 18:28:17 +0300

    eeeeeee booooooy

--- srcs/keys.c
+++ srcs/keys.c
@@ -27,28 +27,26 @@ void	keys(t_pool *pool)
 		turn_right(pool);
 	if (pool->sdl->keyboard_state[SDL_SCANCODE_LEFT] || pool->sdl->keyboard_state[SDL_SCANCODE_A])
 		turn_left(pool);
-	if (pool->sdl->event.type == SDL_KEYDOWN)
+	if (pool->sdl->keyboard_state[SDL_SCANCODE_1])
 	{
-		if (pool->sdl->keyboard_state[SDL_SCANCODE_1])
-		{
-			pool->weapon->act_pistol = 0;
-			pool->weapon->act_knife = 1;
-			pool->bar->act_fuck = 0;
-		}
-		if (pool->sdl->keyboard_state[SDL_SCANCODE_2] && pool->weapon->act_pistol == 0)
-		{
-			pool->weapon->act_pistol = 1;
-			pool->weapon->act_knife = 0;
-			pool->bar->act_fuck = 0;
-		}
-		if (pool->sdl->keyboard_state[SDL_SCANCODE_F])
-		{
-			pool->weapon->act_pistol = 0;
-			pool->weapon->act_knife = 0;
-			pool->bar->act_fuck = 1;
-		}
+		pool->weapon->act_pistol = 0;
+		pool->weapon->act_knife = 1;
+		pool->bar->act_fuck = 0;
 	}
-
+	if (pool->sdl->keyboard_state[SDL_SCANCODE_2] && pool->weapon->act_pistol == 0)
+	{
+		pool->weapon->act_pistol = 1;
+		pool->weapon->act_knife = 0;
+		pool->bar->act_fuck = 0;
+	}
+	if (pool->sdl->event.type == SDL_KEYDOWN && pool->sdl->event.key.keysym.scancode == SDL_SCANCODE_F)
+	{
+		pool->weapon->act_pistol = 0;
+		pool->weapon->act_knife = 0;
+		pool->bar->act_fuck = 1;
+	}
+	if (pool->sdl->event.type == SDL_KEYUP && pool->sdl->event.key.keysym.scancode == SDL_SCANCODE_F)
+		pool->bar->act_fuck = 0;
 	if (pool->sdl->event.key.keysym.scancode == SDL_SCANCODE_ESCAPE)
 		pool->sdl->done = SDL_TRUE;
 }
